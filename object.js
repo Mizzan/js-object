2a46d9d83f6f646bec53595233ce925e1248d609
Mizanur Rahman <mizzanur007@gmail.com>
2021-02-05 21:39:29 +0600

    using and binding other objects with .bind()

--- object.js
+++ object.js
@@ -12,4 +12,18 @@ const userInfo = {
   },
 };
 
-console.log(userInfo.rentPays(2000));
+// console.log(userInfo.rentPays(2000));
+
+const newUserOneInfo = {
+  firstName: "Martin",
+  lastName: "Josef",
+  monthlySalary: 12000,
+};
+const newUserTwoInfo = {
+  firstName: "Ralf",
+  lastName: "Rick",
+  monthlySalary: 14000,
+};
+
+const newUserPayBill = userInfo.rentPays.bind(newUserOneInfo);
+console.log(newUserPayBill(1500));
