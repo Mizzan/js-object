ed289a96ebcf325c2b2c32f0b72e8685681eda66
Mizanur Rahman <mizzanur007@gmail.com>
2021-02-06 22:09:43 +0600

    using .call and calling the functions instance

--- object.js
+++ object.js
@@ -6,8 +6,8 @@ const userInfo = {
     this.monthlySalary = this.monthlySalary - amount;
     return this.monthlySalary;
   },
-  buysFood: function (amount) {
-    this.monthlySalary = this.monthlySalary - amount;
+  buysFood: function (amount, tips, tax) {
+    this.monthlySalary = this.monthlySalary - amount - tips - tax;
     return this.monthlySalary;
   },
 };
@@ -25,5 +25,12 @@ const newUserTwoInfo = {
   monthlySalary: 14000,
 };
 
-const newUserPayBill = userInfo.rentPays.bind(newUserOneInfo);
-console.log(newUserPayBill(1500));
+// we need to assign the function and create an instance of that function for doing so using .bind
+// const newUserPayBill = userInfo.rentPays.bind(newUserOneInfo);
+// console.log(newUserPayBill(1500));
+
+// .call and .apply both works pretty same whereas
+// .call starts with c and c stats with comma, arguments passed in with comma in call
+// but with .call we can directly use that function without creating an instance
+userInfo.buysFood.call(newUserTwoInfo, 3000, 50, 100);
+console.log(newUserTwoInfo.monthlySalary);
